c115bc2d6503c345dee8298d1b597cec53c5eff7
Michael Stucki <orange.urn@gmail.com>
2018-06-23 15:24:40 -0600

    refactored bottles_99.rb

--- ch07/bottles_99.rb
+++ ch07/bottles_99.rb
@@ -1,21 +1,13 @@
 bottles = 99
 while bottles > 0
-  if bottles > 1
-    puts bottles.to_s + " bottles of beer on the wall!"
-    puts bottles.to_s + " bottles of beer!"
-    puts "Take one down! Pass it around!"
-    bottles -= 1
-    if bottles == 1
-      puts bottles.to_s + " bottle of beer on the wall!"
-    else
-      puts bottles.to_s + " bottles of beer on the wall!"
-    end
-  else
-    puts bottles.to_s + " bottle of beer on the wall!"
-    puts bottles.to_s + " bottle of beer!"
-    puts "Take one down! Pass it around!"
-    bottles -= 1
+  puts "#{bottles} #{bottles  == 1 ? "bottle" : "bottles"} of beer on the wall!" 
+  puts "#{bottles} #{bottles  == 1 ? "bottle" : "bottles"} of beer!" 
+  puts "Take one down! Pass it around!"
+  bottles -= 1
+  if bottles == 0
     puts "No more bottles of beer on the wall!"
+  else
+    puts "#{bottles} #{bottles  == 1 ? "bottle" : "bottles"} of beer on the wall!" 
   end
-  puts ""
+  puts ''
 end
