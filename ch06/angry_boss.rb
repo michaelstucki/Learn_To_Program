a8914ebd5fd96c1cf46c83a771c09dc073e7bfc1
Michael Stucki <orange.urn@gmail.com>
2018-06-23 10:42:39 -0600

    added ch06 exercises

---
+++ ch06/angry_boss.rb
@@ -0,0 +1,9 @@
+puts "What do you want to ask your boss for?"
+request = gets.chomp
+
+reply = "Are you insane?"
+reply += " There's no way you're getting any " + request + "!"
+reply += " Now pack up your junk and get out of here! You're fired, you fool!!"
+
+puts reply.upcase
+
---
+++ ch06/table_of_contents.rb
@@ -0,0 +1,9 @@
+puts "Table of Contents".center(50)
+puts ""
+puts "Chapter 1: Getting Started".ljust(30) + "page 1".rjust(10)
+puts ""
+puts "Chapter 2: Numbers".ljust(30) + "page 9".rjust(10)
+puts ""
+puts "Chapter 3: Letters".ljust(30) + "page 13".rjust(10)
+
+
