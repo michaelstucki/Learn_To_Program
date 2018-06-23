229deb6b715adf5b60c19702073cab79e7851229
Michael Stucki <orange.urn@gmail.com>
2018-06-23 09:38:48 -0600

    added ch05 exercises

---
+++ ch05/favorite_number.rb
@@ -0,0 +1,6 @@
+puts "Enter your favorite number"
+number = gets.chomp.to_i
+number += 1
+
+puts "OK, here's a bigger, better favorite number: " + number.to_s
+
---
+++ ch05/full_name_greeting.rb
@@ -0,0 +1,10 @@
+puts "Enter your first name"
+first_name = gets.chomp
+puts "Enter your middle name"
+middle_name = gets.chomp
+puts "Enter your last name"
+last_name = gets.chomp
+
+full_name = first_name + ' ' + middle_name + ' ' + last_name
+puts "Hello, " + full_name + "!"
+
